85a9a7a8b01415dafd8243b7292aa4eff86f8b3b
Heyho-letsgo <abaudin@gmail.com>
2014-01-02 15:08:02 +0100

    front-end framework

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -2,9 +2,6 @@ class VisitorsController < ApplicationController
 
   def new
     @owner = Owner.new
-    flash.now[:alert] = 'Welcome!'
-    flash.now[:notice] = 'My birthday is soon.'
-  	#render 'visitors/new'
   end
 
 end
