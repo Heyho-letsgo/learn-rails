c41f2a7505c5686dfe6b7ec9df73ef5a63a407f4
André Baudin-Laurencin <abaudin@gmail.com>
2014-10-07 15:58:13 +0200

    Page accueil ok mailchimp non la cle doit etre cachee

--- app/models/visitor.rb
+++ app/models/visitor.rb
@@ -19,25 +19,6 @@ class Visitor < ActiveRecord::Base
 
 
 
-
-  def subscribe
-    mailchimp = Gibbon::API.new
-    result = mailchimp.lists.subscribe({
-      :id => ENV['MAILCHIMP_LIST_ID'],
-      :email => {:email => self.email},
-      :double_optin => false,
-      :update_existing => true,
-      :send_welcome => true
-    })
-    Rails.logger.info("Subscribed #{self.email} to MailChimp") if result
-  end
-
-
-
-
-
-
-
 end
 
 
--- config/initializers/mailchimp.rb
+++ config/initializers/mailchimp.rb
@@ -2,7 +2,7 @@ if Rails.env.test?
   Gibbon::Export.api_key = "fake"
   Gibbon::Export.throws_exceptions = false
 end
-Gibbon::API.api_key = "440798c8b9bb96676411ae5279b0637e-us3"
+Gibbon::API.api_key = "YOUR_API_KEY"
 Gibbon::API.timeout = 15
 Gibbon::API.throws_exceptions = false
 Rails.configuration.mailchimp = Gibbon::API.new 
