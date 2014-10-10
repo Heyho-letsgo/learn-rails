16a7dd13d3899d13369d3168656b157ade8cd253
André Baudin-Laurencin <abaudin@gmail.com>
2014-10-10 08:52:41 +0200

    mailchimp config

--- config/initializers/mailchimp.rb
+++ config/initializers/mailchimp.rb
@@ -2,7 +2,3 @@ if Rails.env.test?
   Gibbon::Export.api_key = "fake"
   Gibbon::Export.throws_exceptions = false
 end
-Gibbon::API.api_key = "YOUR_API_KEY"
-Gibbon::API.timeout = 15
-Gibbon::API.throws_exceptions = false
-Rails.configuration.mailchimp = Gibbon::API.new 
